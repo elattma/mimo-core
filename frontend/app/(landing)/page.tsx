5b1de09b73c6acbf127760259eecdef5cc151ee7
Henry <henryapereira00@gmail.com>
2023-03-23 09:47:10 -0700

    fix: correct use of Balancer

--- frontend/app/(landing)/page.tsx
+++ frontend/app/(landing)/page.tsx
@@ -14,17 +14,15 @@ export default function LandingPage() {
     <>
       <section className="container flex flex-col items-center justify-center gap-theme py-theme md:flex-row">
         <div className="flex flex-col items-center gap-theme-3/4 md:w-fit md:items-start md:gap-theme lg:w-128">
-          <Balancer ratio={0.75}>
-            <h1 className="text-center text-5xl font-semibold leading-tight text-neutral-text-contrast md:text-start lg:text-6xl">
-              Lorem ipsum dolor sit amet.
-            </h1>
-          </Balancer>
-          <Balancer ratio={0.5}>
-            <p className="text-center text-xl font-medium text-neutral-text md:text-start lg:text-2xl">
+          <h1 className="text-center text-5xl font-semibold leading-tight text-neutral-text-contrast md:text-start lg:text-6xl">
+            <Balancer ratio={0.75}>Lorem ipsum dolor sit amet.</Balancer>
+          </h1>
+          <p className="text-center text-xl font-medium text-neutral-text md:text-start lg:text-2xl">
+            <Balancer ratio={0.5}>
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut sunt
               fugit cumque.
-            </p>
-          </Balancer>
+            </Balancer>
+          </p>
           <Dialog>
             <DialogTrigger asChild>
               <Button className="w-fit" size="lg">
