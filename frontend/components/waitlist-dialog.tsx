f523887471195cc99d19613791ed47cf79dd8849
Henry <henryapereira00@gmail.com>
2023-03-28 19:29:26 -0700

    fix: close waitlist dialog after submission

--- frontend/components/waitlist-dialog.tsx
+++ frontend/components/waitlist-dialog.tsx
@@ -2,20 +2,23 @@
 
 import { Button } from "@/components/ui/button";
 import { DialogClose } from "@/components/ui/dialog";
-import { FormEventHandler, useState } from "react";
+import { FormEventHandler, useRef, useState } from "react";
 
 export default function WaitlistForm() {
+  const close = useRef<HTMLButtonElement>(null);
   const [email, setEmail] = useState<string>("");
 
   const handleSubmit: FormEventHandler = (event) => {
+    event.preventDefault();
     fetch("/api/waitlist", {
       method: "POST",
       body: JSON.stringify({ message: email }),
       next: { revalidate: 1 },
     });
+    close.current?.click();
   };
   return (
-    <form className="flex flex-col">
+    <form className="flex flex-col" onSubmit={handleSubmit}>
       <label className="mb-theme-1/8 text-sm">Email</label>
       <input
         className="mb-theme rounded-theme border border-neutral-border bg-transparent p-theme-1/4 text-gray-text placeholder:text-gray-10"
@@ -24,10 +27,11 @@ export default function WaitlistForm() {
         value={email}
         onChange={(event) => setEmail(event.target.value)}
       />
-      <DialogClose asChild>
-        <Button type="submit" className="w-fit self-end" onClick={handleSubmit}>
-          Join
-        </Button>
+      <Button type="submit" className="w-fit self-end">
+        Join
+      </Button>
+      <DialogClose className="hidden" aria-hidden asChild>
+        <button ref={close} />
       </DialogClose>
     </form>
   );
